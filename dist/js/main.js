3a1f76b45c44ff20eeed0de60b167e956220f376
prince-conte <prince-conte@outlook.com>
2016-09-19 19:13:01 +0300

    yap

---
+++ dist/js/main.js
@@ -0,0 +1,26 @@
+$(document).on('ready', function() {
+      $(".regular").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });      
+    $(".regular2").slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4
+      });
+      $(".center-slider").slick({
+        dots: true,
+        infinite: true,
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      });
+      $(".variable").slick({
+        dots: true,
+        infinite: true,
+        variableWidth: true
+      });
+    });
